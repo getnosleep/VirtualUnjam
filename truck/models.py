f326d40c315cc7ddd2fae8bb87f00cacf5cbdb0d
Shooty215 <b.heinr@web.de>
2021-04-07 03:13:21 +0200

    Implemented first draft of trucks' service class. Service class holds functions: 		 changeTruckIdentificator(truckId, newTruckId), changeConvoyPosition(truckId, newConvoyPosition),                  changeConvoyLeader(truckId, newConvoyLeaderId), changeSpeed(truckId, speedOffset) Functions validate input datatypes and signal success and failure via returning true or false. Futhermore changeSpeed() evaluates new speed and min/max speed of the truck to set new speed.
    Added docstring and commentary to models.py.
    Adjusted truck model's properties:
    		id Primary_key, truckId Integer, convoyPosition Integer, convoyLeaderId Integer, maxSpeed Float, speed Float.
    
    Added new validation library and deleted the old one from requierments.txt:
    		added validation==0.6.1
    		deleted annotation_validation==0.1.1

--- truck/models.py
+++ truck/models.py
@@ -1,12 +1,21 @@
+"""[Docstring] Declares trucks' model."""
+# Imports
 from django.db import models
-
+# Declare models as held in array.
 __all__ = []
-
+# Truck model class.
 class Truck(object):
+    """[Docstring] Declares truck model's properties and their data types.
+
+    Properties: id = Primary_key, truckId = Integer, convoyPosition = Integer, convoyLeaderId = Integer, maxSpeed = Float, speed = Float.
+    """
+    
+    # Declare truck model's properties and datatypes.
     id = models.AutoField(primary_key=True)
-    truck_identificator = models.TextField()
-    convoy_position = models.IntegerField()
-    leader_convoy_position = models.IntegerField()
+    truckId = models.IntegerField()
+    convoyPosition = models.IntegerField()
+    convoyLeaderId = models.IntegerField()
+    maxSpeed = models.IntegerField()
     speed = models.FloatField()
     #truck_in_front = models.OneToOneField()
     #truck_behind = models.OneToOneField()
--- truck/services.py
+++ truck/services.py
@@ -1,5 +1,65 @@
-from .models import Truck
-
+"""[Docstring] Declares trucks' service class."""
+# Imports
+from . import models
+import validation
+# Trucks' service class.
 class service:
-    def changeSpeed(self, truck):
-        pass
+    """[Docstring] Declares functions altering trucks' properties.
+    
+    Functions: changeTruckIdentificator(truckId, newTruckId), changeConvoyPosition(truckId, newConvoyPosition),
+               changeConvoyLeader(truckId, newConvoyLeaderId), changeSpeed(truckId, speedOffset)
+    """
+
+    def changeTruckIdentificator(self, truckId, newTruckId):
+        """[Docstring] Change identificator of a truck.
+        
+        Inputs: Integer truckId, Integer newTruckId.
+        """
+        # Id validation: If truck's old and new id are integers, set new id.
+        if validation.validate_int(truckId) and validation.validate_int(newTruckId):
+            # Set new id on truck object referenced by the old id.
+            models.Truck.objects.get(truckId=truckId).truckId = newTruckId
+
+    def changeConvoyPosition(self, truckId, newConvoyPosition):
+        """[Docstring] Change convoy position reference of a truck.
+        
+        Inputs: Integer truckId, Integer newConvoyPosition.
+        """
+        # Position validation: If truck's id and new position are integers (unnecessary: and the truck's new position is not the old one), set new position in convoy.
+        if validation.validate_int(truckId) and validation.validate_int(newConvoyPosition):
+            # Set new position on truck object referenced by its' truck id.
+            models.Truck.objects.get(truckId=truckId).convoyPosition = newConvoyPosition
+        
+    def changeConvoyLeader(self, truckId, newConvoyLeaderId):
+        """[Docstring] Change convoy's leader reference of a truck.
+        
+        Inputs: Integer truckId, Integer newConvoyLeaderId.
+        """
+        # Leader validation: If truck's id and the new convoy leader's id are integers, set new leaders position.
+        if validation.validate_int(truckId) and validation.validate_int(newConvoyLeaderId):
+            # Set new convoy leader's id on truck object referenced by its' truck id.
+            models.Truck.objects.get(truckId=truckId).convoyLeaderId = newConvoyLeaderId
+        
+    def changeSpeed(self, truckId, speedOffset):
+        """[Docstring] Change driving speed of a truck.
+        
+        Inputs: Integer truckId, Integer speedOffset.
+        """
+        # Speed validation: If truck's id is an integer, the speed offset is a float, set truck's speed, according to its' max speed.
+        if validation.validate_int(truckId) and validation.validate_float(speedOffset):
+            # Set truck's speed, according to its' max speed, referenced by its' id.
+            # bind new speed and max speed of the truck for performance reasons
+            newSpeed = float(speedOffset + models.Truck.objects.get(truckId = truckId).speed)
+            maxSpeed = float(models.Truck.objects.get(truckId = truckId).maxSpeed)
+            # If new speed is inbetween 0 and max speed, set new speed.
+            if newSpeed < maxSpeed and newSpeed >= 0:
+                # Set new speed on truck object referenced by its' id.
+                models.Truck.objects.get(truckId = truckId).speed += speedOffset
+            # If new speed is lower 0, set 0 as new speed.
+            elif newSpeed < 0:
+                # Set new speed on truck object accordingly, referenced by its' id
+                models.Truck.objects.get(truckId = truckId).speed = 0
+            # If new speed is above truck's max speed, set max speed as new speed
+            elif newSpeed > maxSpeed:
+                # Set new speed on truck object accordingly, referenced by its' id
+                models.Truck.objects.get(truckId = truckId).speed = maxSpeed
