571b2cdb2c5f812e0d045656bfb2c26c9496a3d3
Shooty215 <b.heinr@web.de>
2021-04-11 02:44:14 +0200

    Rewritten voting function to work recursively, list of trucks to send initiation to can't be none anymore. No commentary on these functions yet.

--- truck/services.py
+++ truck/services.py
@@ -32,7 +32,7 @@ class Service(object):
             determineTruckBehind(truckId):boolean to be edited
 
             adjustSpeedOfTruckToLeader(truckId):
-            
+
             initiateVote(truckId):boolean
 
             pollingWithAnotherTruck(pollingTruckId):boolean
@@ -49,7 +49,6 @@ class Service(object):
     truckAPIPort: Final[str]   = '8000'
 
     # Function validating truck object's structure.
-    #def validateTruckObjectStructure(self, truckId: int, maxSpeed: float):
     @staticmethod
     def validateTruckObjectStructure(truckId: int):
         """[Docstring] Validates structure of a truck object.
@@ -200,8 +199,9 @@ class Service(object):
         # Set new convoy position as truck's respective reference.
         truck.convoyPosition.set(newConvoyPosition)
         # Save the truck and bind.
-        # Bind truck's object to reference.
-        truck = Truck.objects.get(truckId=truckId) == newConvoyPosition
+        val = truck.save(force=True)
+        # validate new position in convoy.
+        return val.convoyPosition == newConvoyPosition
 
     # Function changing truck's convoy leader reference.
     @staticmethod
@@ -328,16 +328,11 @@ class Service(object):
         # Calculate truck's new speed.
         newSpeed = truck.speed + speedOffset
         # If truck's new speed is inbetween 0 and maxSpeed, set newSpeed as truck's speed.
-        if validate_float(min_value=0, max_value=Service.maxSpeed,  value=newSpeed):
-            pass
+        if validate_float(min_value=0, max_value=Service.maxSpeed,  value=newSpeed): pass
         # If new speed is lower 0, 0 will be as new speed.
-        elif not validate_float(min_value = 0, value = newSpeed):
-            # Bind new speed to reference for validation.
-            newSpeed = 0
+        elif not validate_float(min_value = 0, value = newSpeed): newSpeed = 0
         # If new speed is above truck's max speed, max speed will be set as new speed.
-        elif not validate_float(max_value=Service.maxSpeed, value=newSpeed):
-            # Set maxSpeed as newSpeed.
-            newSpeed = Service.maxSpeed
+        elif not validate_float(max_value=Service.maxSpeed, value=newSpeed): newSpeed = Service.maxSpeed
         # Set evaluated newSpeed as truck's speed.
         truck.speed.set(newSpeed)
         # Save the truck and bind it to a reference.
@@ -350,33 +345,33 @@ class Service(object):
     def adjustSpeedToTruck(truckId: int, idolTruckId: int):
         """[Docstring] Adjusts speed of the truck to another truck.
 
-            Inputs:
+        Inputs:
 
-                Integer truckId, Float speedOffset.
+            Integer truckId, Float speedOffset.
 
-            Results:
+        Results:
 
-                True        -   In case the truck's new speed could be set.
+            True        -   In case the truck's new speed could be set.
 
-                False       -   In case the update validation went wrong.
+            False       -   In case the update validation went wrong.
 
-                Exception   -   In case the input validation went wrong.
+            Exception   -   In case the input validation went wrong.
 
-            Logic:
+        Logic:
 
-                1.  Validates Inputs.
+            1.  Validates Inputs.
 
-                2.1 Binds truck's model object to variable referenced by its' truckId.
+            2.1 Binds truck's model object to variable referenced by its' truckId.
 
-                2.2 Binds idol's model object to variable referenced by its' truckId.
+            2.2 Binds idol's model object to variable referenced by its' truckId.
                     
-                3.  Sets truck's speed to idol's speed.
+            3.  Sets truck's speed to idol's speed.
 
-                4.  Validates value update.
+            4.  Validates value update.
 
-                4.1 Signals success on valid update, via returning true.
+            4.1 Signals success on valid update, via returning true.
 
-                4.2 Signals failure on invalid update, via returning false.
+            4.2 Signals failure on invalid update, via returning false.
         """
         # Bind truck's object to reference.
         truck = Truck.objects.get(truckId=truckId)
@@ -389,135 +384,46 @@ class Service(object):
         # Validate update via bound truck object and input.
         return val.speed == idolTruck.speed
 
-    # Function initiating votes among higher identified trucks.
-    @staticmethod
-    def initiateVote(truckId: int, convoyLeaderId: int, toBullyTruckIds: list([int]) or None):
-        """[Docstring] Initiate vote among higher identified trucks.
-
-            Inputs:
-
-                Integer truckId.
-
-            Results:
-
-                True        -   In case the votes are initiated successfully.
-
-                List        -   In case unreachable services are encountered.
-
-                False       -   In case an unexpected error is registered.
-
-                Exception   -   In case the input validation went wrong.
-
-            Logic:
-
-                1.  Validates Inputs.
-
-                2.  Binds truck's model object to variable referenced by its' truckId.
-                    
-                3.1 Will create new list, containing all truckId to bully, if none is provided.
-
-                3.2 Will use input list, if it is provided on function call.
-
-                4.1 Bullies votes on referenced truck's.
-
-                4.2 Keeps bullying trucks referenced in bullying list, until all services signal 
-                    success or maximum number of connection attempts are reached.
-
-                5.1 Signals successfully bullying every truck, via returning true.
-
-                5.2 Signals failure, via returning a list of unreachable services.
-
-                5.3 Signals unknow error, via returning false.
-        """
-        try:
-            # Bind truck's object to reference.
-            truck = Truck.objects.get(truckId=truckId)
-            # If no reserved list is provided to function call, will create one as range for loop.
-            if toBullyTruckIds == None: toBullyTruckIds = list(range(truck.truckId, convoyLeaderId)).reverse()
-            else: pass
-            # define time out counter
-            availabilityValidation = 0
-            # Initialize vote on higher identified trucks in convoy.
-            while toBullyTruckIds:
-                # Increase availability token.
-                availabilityValidation += 1
-                # If loop tried to reach to trucks too often, will throw exception to execute final block
-                if availabilityValidation > 5: raise Exception(status.HTTP_503_SERVICE_UNAVAILABLE)
-                # Iterate available truckIds and try to initiate a vote on referenced truck, via request
-                for x in toBullyTruckIds:
-                    # file post request for truck json against api
-                    resp = requests.post(
-                        'http://'
-                        + Service.truckAPIHost + ':'
-                        + Service.truckAPIPort + '/api/truck/'
-                        + x + '/vote/',
-                        params=dict(
-                            higherIdentifiedTrucks=str(
-                                toBullyTruckIds
-                            )
-                        )
-                    )
-                    # Will evaluate response status code to decide, if the truckId can be deleted from to-bully-truckIds-list
-                    # or has to be kept for further requests.
-                    if    resp.status_code == status.HTTP_200_OK:                    toBullyTruckIds.remove(x)
-                    elif  resp.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR: pass
-                    elif  resp.status_code == status.HTTP_501_NOT_IMPLEMENTED:       pass
-                    elif  resp.status_code == status.HTTP_502_BAD_GATEWAY:           pass
-                    else:                                                            pass
-            # If all bullied trucks signaled vote acknowledgement, will bind status code 200 - OK to string provided on function call.
-            if not toBullyTruckIds: statusCode = str(status.HTTP_200_OK)
-            else:                   raise Exception(status.HTTP_503_SERVICE_UNAVAILABLE)
-        # Catch status code on failure exception
-        except Exception as e:
-            statusCode = str(e)
-        finally:
-            # Will provide success to function call, if evaluated status code is 200 - OK.
-            # Will provide list of unavailable services' truckIds to function call, if evaluated status code is 503 - SERVICE UNAVAILABLE.
-            # Will provide failure to function call, if an unexpected error accured.
-            if    statusCode == status.HTTP_200_OK:                  return True
-            elif  statusCode == status.HTTP_503_SERVICE_UNAVAILABLE: return toBullyTruckIds
-            else:                                                    return False
-
     # Function polling two truck's via truckId reference
     @staticmethod
     def pollingWithAnotherTruck(truckId: int, pollingTruckId: int):
         """[Docstring] Polling in between truck and a another one.
 
-            Inputs:
+        Inputs:
 
-                Integer truckId, Integer pollingTruckId.
+            Integer truckId, Integer pollingTruckId.
 
-            Results:
+        Results:
 
-                status.HTTP_404_NOT_FOUND  -   In case the referenced polling truck does not exist.
+            status.HTTP_404_NOT_FOUND  -   In case the referenced polling truck does not exist.
                 
-                status.HTTP_200_OK         -   In case the polling is successful.
+            status.HTTP_200_OK         -   In case the polling is successful.
 
-                status.HTTP_403_FORBIDDEN  -   In case the polling is a failure.
+            status.HTTP_403_FORBIDDEN  -   In case the polling is a failure.
 
-                Exception                  -   In case the input validation went wrong.
+            Exception                  -   In case the input validation went wrong.
 
-            Logic:
+        Logic:
 
-                1.    Validates Inputs.
+            1.    Validates Inputs.
 
-                2.    Binds polling truck's model object to variable referenced by its' truckId.
+            2.    Binds polling truck's model object to variable referenced by its' truckId.
                     
-                3.    Evaluate Polling.
+            3.    Evaluate Polling.
                 
-                3.1.0 If the polling truck's truckId is bigger, it is not broken, will evaluate
-                      polling as lost.
+            3.1.0 If the polling truck's truckId is bigger, it is not broken, will evaluate
+                polling as lost.
 
-                3.1.1 Signals null reference, via returning status.HTTP_404_NOT_FOUND.
+            3.1.1 Signals null reference, via returning status.HTTP_404_NOT_FOUND.
                 
-                3.2.1 If the polling truck's truckId is bigger, but it's broken, 
-                      will evaluate polling as won.
+            3.2.1 If the polling truck's truckId is bigger, but it's broken, 
+                  will evaluate polling as won.
 
-                3.2.2 Signals success, via returning status.HTTP_200_OK.
+            3.2.2 Signals success, via returning status.HTTP_200_OK.
                 
-                3.3.1 If it is not in existence, will evaluate polling as won.
+            3.3.1 If it is not in existence, will evaluate polling as won.
 
-                3.3.2 Signals failure, via returning status.HTTP_403_FORBIDDEN.
+            3.3.2 Signals failure, via returning status.HTTP_403_FORBIDDEN.
         """
         # Bind polling truck's model object and truck's model object, referenced by their truckId.
         val = Truck.objects.get(truckId=pollingTruckId)
@@ -532,40 +438,40 @@ class Service(object):
     def pollingWithHigherIdentifiedTrucks(truckId: int, pollingTruckIds: list([int])):
         """[Docstring] Initiate vote among polling trucks.
 
-            Inputs:
+        Inputs:
 
-                Integer truckId, List pollingTruckIds.
+            Integer truckId, List pollingTruckIds.
             
-            Results:
+        Results:
 
-                True        -   In case the polling is successful.
+            True        -   In case the polling is successful.
 
-                False       -   In case the polling is a failure.
+            False       -   In case the polling is a failure.
 
-                Exception   -   In case the input validation went wrong.
+            Exception   -   In case the input validation went wrong.
 
-            Logic:
+        Logic:
 
-                1.    Validates Inputs.
+            1.    Validates Inputs.
 
-                2.    Binds polling truck's model object to variable referenced by its' truckId.
+            2.    Binds polling truck's model object to variable referenced by its' truckId.
                     
-                3.    Iterate list of integers provided by the function call, via a for loop.
+            3.    Iterate list of integers provided by the function call, via a for loop.
                 
-                3.1.0 Polls with respective truck, for each integer in provided list.
+            3.1.0 Polls with respective truck, for each integer in provided list.
 
-                3.1.1 If polling failed, stop truck from further polling.
+            3.1.1 If polling failed, stop truck from further polling.
 
-                3.1.2 If polling is successful, will keep polling.
+            3.1.2 If polling is successful, will keep polling.
 
-                3.1.3 If polling is succesful but the next truck isn't exisiting, will assume
-                      convoy leadership.
+            3.1.3 If polling is succesful but the next truck isn't exisiting, will assume
+                  convoy leadership.
                 
-                4.    Signals polling evaluation.
+            4.    Signals polling evaluation.
 
-                4.1.0 Will signal success, if convoy leadership is optained.
+            4.1.0 Will signal success, if convoy leadership is optained.
 
-                4.1.1 Will signal failure, if further polling is stopped.
+            4.1.1 Will signal failure, if further polling is stopped.
         """
         # Bind truck's model object, referenced by its' their truckId.
         truck = Truck.objects.get(truckId=truckId)
@@ -579,3 +485,35 @@ class Service(object):
             elif result == status.HTTP_403_FORBIDDEN: break
         # Validate truck's convoy leadership.
         return Service.validateConvoyLeadership(truck.truckId)
+
+    @staticmethod
+    def initiateOneVote(toBullyTruckId: int, connectionAttempt: int, toBullyTruckIds:list([int])):
+        try:
+            # Increase connectionAttempt by one.
+            connectionAttempt += 1
+            # File post request for truck json against api.
+            resp = requests.post(
+                                'http://'
+                                + Service.truckAPIHost + ':'
+                                + Service.truckAPIPort + '/api/truck/'
+                                + toBullyTruckId + '/vote/',
+                                params=dict(
+                                    higherIdentifiedTrucks=str(
+                                        toBullyTruckIds
+                                    )
+                                )   
+                            )
+            # Will evaluate response status code to decide, if the truckId can be deleted from to-bully-truckIds-list
+            # or has to be kept for further requests.
+        except: pass
+        finally: 
+            if resp.status_code == status.HTTP_200_OK: return True
+            elif connectionAttempt > 5:                return False
+            else: Service.initiateOneVote(toBullyTruckId, connectionAttempt, toBullyTruckIds)
+    
+    @staticmethod
+    def initiateAllVotes(truckId: int, convoyLeaderId: int, toBullyTruckIds: list([int])):
+        for x in toBullyTruckIds: 
+            if Service.initiateOneVote(x, 0, toBullyTruckIds): toBullyTruckIds.remove(x)
+            else:                                              pass
+        return toBullyTruckIds        
