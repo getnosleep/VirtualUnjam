06b4a82caf638307c045f634af11d02257446552
Alexander Schnurbus <alexander-schnurbus@outlook.de>
2021-04-23 18:28:26 +0200

    Functionality for references implemented
    The functions of the convoy references are implemented now

--- address/views.py
+++ address/views.py
@@ -6,32 +6,51 @@ import json
 
 REGISTERED = {} # {1: '192.168.0.2:5001'}
 
-class ConvoyViewSet(viewsets.ViewSet):
-    """
-    def create(self, request):
-        convoy = request.convoy
-        try:
-            serializer = ConvoySerializer(data=convoy)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        except:
-            return Response(data=convoy, status=status.HTTP_406_NOT_ACCEPTABLE)
-    """
+def __extractTruckIds__():
+    """@return truckIds from REGISTERED"""
+    keys = REGISTERED.keys()
+    truckIds = []
+    for key in keys:
+        truckIds.append(key)
+    return truckIds
 
+def __checkEntry__(dictionary, key, value):
+    """@return success for correct value"""
+    if __checkKey__(dictionary, key):
+        val = dictionary[key]
+        if val == value:
+            return True
+    return False
+
+def __checkKey__(dictionary, key):
+    if key in dictionary.keys():
+        val = dictionary[key]
+        return True
+    return False
+
+class ConvoyViewSet(viewsets.ViewSet):
     def data(self, request):
         """@return list of adresses of the trucks in this convoy"""
         return Response(data=REGISTERED, status=status.HTTP_200_OK)
     
     def truckIds(self, request):
         """@return list of ids of the trucks in this convoy"""
-        truckIds = []
-        return Response(data=truckIds, status=status.HTTP_200_OK)
+        try:
+            truckIds = __extractTruckIds__()
+            return Response(data=truckIds, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
     
-    def address(self, request, truckId: int):
+    def address(self, request):
         """@return resolved networkadress for the requested truckId"""
         try:
-            address = ''
+            data = json.loads(request.body)
+            truckId = data['truckId']
+            
+            if not __checkKey__(REGISTERED, truckId):
+                raise Exception()
+
+            address = REGISTERED[truckId]
             return Response(data=address, status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -41,22 +60,25 @@ class ConvoyViewSet(viewsets.ViewSet):
         try:
             data = json.loads(request.body)
             truckId = data['truckId']
+    	    if __checkKey__(REGISTERED, truckId):
+                raise Exception()
             address = data['address']
             REGISTERED.update({truckId: address})
-            return Response(data=REGISTERED, status=status.HTTP_200_OK)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        """
-        try:
             return Response(status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        """
 
-    def leave(self, request, truckId: int):
+    def leave(self, request):
         """@return bool success"""
         try:
+            data = json.loads(request.body)
+            truckId = data['truckId']
+            address = data['address']
+
+            if not __checkEntry__(REGISTERED, truckId, address):
+                raise Exception()
+
+            del REGISTERED[truckId]
             return Response(status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
