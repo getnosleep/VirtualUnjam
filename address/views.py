ed3f11f916d792f79e622e866d8ac771cd96e7c4
Shooty215 <b.heinr@web.de>
2021-05-08 19:13:02 +0200

    test

--- address/views.py
+++ address/views.py
@@ -4,39 +4,40 @@ from rest_framework.response import Response
 
 import json
 
-REGISTERED = {} # {1: '192.168.0.2:5001'}
-
-def __extractTruckIds__():
-    """@return truckIds from REGISTERED"""
-    keys = REGISTERED.keys()
-    truckIds = []
-    for key in keys:
-        truckIds.append(key)
-    return truckIds
-
-def __checkEntry__(dictionary, key, value):
-    """@return success for correct value"""
-    if __checkKey__(dictionary, key):
-        val = dictionary[key]
-        if val == value:
-            return True
-    return False
-
-def __checkKey__(dictionary, key):
-    if key in dictionary.keys():
-        val = dictionary[key]
-        return True
-    return False
-
 class ConvoyViewSet(viewsets.ViewSet):
+
+    REGISTERED = {}  # {1: '192.168.0.2:5001'}
+
+
+    def __extractTruckIds__(self):
+        """@return truckIds from REGISTERED"""
+        keys = ConvoyViewSet.REGISTERED.keys()
+        truckIds = []
+        for key in keys:
+            truckIds.append(key)
+        return truckIds
+
+    def __checkEntry__(self, dictionary, key, value):
+        """@return success for correct value"""
+        if ConvoyViewSet.__checkKey__(self, dictionary, key):
+            val = dictionary[key]
+            if val == value:
+                return True
+        return False
+
+    def __checkKey__(self, dictionary, key):
+        if key in dictionary.keys():
+            return True
+        return False
+
     def data(self, request):
         """@return list of adresses of the trucks in this convoy"""
-        return Response(data=REGISTERED, status=status.HTTP_200_OK)
+        return Response(data=ConvoyViewSet.REGISTERED, status=status.HTTP_200_OK)
     
     def truckIds(self, request):
         """@return list of ids of the trucks in this convoy"""
         try:
-            truckIds = __extractTruckIds__()
+            truckIds = ConvoyViewSet.__extractTruckIds__(self)
             return Response(data=truckIds, status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -47,10 +48,10 @@ class ConvoyViewSet(viewsets.ViewSet):
             data = json.loads(request.body)
             truckId = data['truckId']
             
-            if not __checkKey__(REGISTERED, truckId):
+            if not ConvoyViewSet.__checkKey__(self, ConvoyViewSet.REGISTERED, truckId):
                 raise Exception()
 
-            address = REGISTERED[truckId]
+            address = ConvoyViewSet.REGISTERED[truckId]
             return Response(data=address, status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -60,10 +61,10 @@ class ConvoyViewSet(viewsets.ViewSet):
         try:
             data = json.loads(request.body)
             truckId = data['truckId']
-    	    if __checkKey__(REGISTERED, truckId):
+            if ConvoyViewSet.__checkKey__(self, ConvoyViewSet.REGISTERED, key=truckId):
                 raise Exception()
             address = data['address']
-            REGISTERED.update({truckId: address})
+            ConvoyViewSet.REGISTERED.update({truckId: address})
             return Response(status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
@@ -75,10 +76,10 @@ class ConvoyViewSet(viewsets.ViewSet):
             truckId = data['truckId']
             address = data['address']
 
-            if not __checkEntry__(REGISTERED, truckId, address):
+            if not ConvoyViewSet.__checkEntry__(self, ConvoyViewSet.REGISTERED, truckId, address):
                 raise Exception()
 
-            del REGISTERED[truckId]
+            del ConvoyViewSet.REGISTERED[truckId]
             return Response(status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
