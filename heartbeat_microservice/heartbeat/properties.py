8144ec662be76720425555f8650cf3fbdbecc825
Alexander Schnurbus <alexander-schnurbus@outlook.de>
2021-06-17 21:57:40 +0200

    Property change
    @shooty215 Found an error, this will help maybe ;D

---
+++ heartbeat_microservice/heartbeat/properties.py
@@ -0,0 +1,9 @@
+"""Some values the broker needs to know where to publsih and when to publish data"""
+
+# Network Identification: MQTT Broker (self) -> fixed
+INTERVAL = 0.025
+ADDRESS_BROKER = '127.0.0.1'
+PORT_BROKER = 1028
+USERNAME_BROKER = 'testUser'
+PASSWORD_BROKER = 'test'
+CHANNEL_BROKER = 'truckChannel'
--- truck_microservice/truck/properties.py
+++ truck_microservice/truck/properties.py
@@ -1,29 +1,30 @@
-# Truck Microservice -> depending on service
+"""Some values the truck needs to know about the network (Convoy, Admin-Service, and Broker)"""
+
+# Network Identification: Truck (self) Microservice -> depending on truck
 ID = 151
 ADDRESS_SELF = '127.0.0.1:1031'
 
-# Address Microservice -> fixed
+# Network Identification: Convoy Microservice -> fixed
 ADDRESS_CONVOY = '127.0.0.1:1030'
 ADDRESS_BROKER = '127.0.0.1'
 
-# Admin Microservice -> fixed
+# Network Identification: Admin Microservice -> fixed
 ADDRESS_ADMIN = '127.0.0.1:1029'
 
-# MQTT Broker -> fixed
+# Network Identification: MQTT Broker -> fixed
 PORT_BROKER = 1028
 USER_BROKER = 'testUser'
 PASSWORD_BROKER = 'test'
 CHANNEL_BROKER = 'truckChannel'
 DURATION_BROKER = 0.025
 
-# physical fixed props
+# Truck specific properties: Truck physics -> fixed
 MIN_SPEED = 0.0
 MAX_SPEED = 22.22222222222222
+
+# Truck specific properties: Truck physics and calculation -> depending on truck
 MIN_ACCELERATION = -3.0
 MAX_ACCELERATION = 2.5
-EMERGENCY_BRAKE = -2.5
-
-# changeable props
 LENGTH = 18.0
 DISTANCE = 0.5
 DEPARTURE_DISTANCE = 300.0
