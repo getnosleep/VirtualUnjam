53b684f9706be813d2a8b1cd0352934b6f518b9e
Alexander Schnurbus <alexander-schnurbus@outlook.de>
2021-06-18 13:59:10 +0200

    Repush subscriber

--- truck_microservice/truck/daemons/subscriber.py
+++ truck_microservice/truck/daemons/subscriber.py
@@ -12,24 +12,6 @@ class Subscriber(Thread):
         self.__client__: Client
     
     def run(self):
-        """
-        Wenn wir das so machen (run ohne die underscores), sind das sogesehen override-methoden (zumindest wuerde man das in Java so nennen...)
-        Somit koennen wir die normale start-Methode aus dem Thread nehmen um den run automatisch einzuleiten (die hat durch den super-Call ja ne Autoreferenz).
-
-        Darueber hinaus glaube ich, dass es nicht reicht den self.__client__.loop_start() aufzurufen, denn das startet, so wie ich es gesehen habe,
-        einen von dieser Klasse unabhaengigen Daemon. Dieser sorgt dafuer, dass der Subscriber zwar weiter zuhoert, allerdings die Werte nicht in
-        diese Klasse und somit nicht in unseren Microservice zurueck gibt (wobei ich mir hierbei nicht so ganz sicher bin, ob da nicht doch irgendwo
-        ein Hintertuerchen ist oder so, denn es scheint ja doch irgendwe bei dir geklappt zu haben ... Ich bin allerdings auch muede ... und
-        vielleicht laber ich grade scheisse xD )
-
-        Wie dem auch sei... morgen geht es weiter :D
-
-        DIE LIBRARY MACHT DAS SO. die callback funktionen müssen statisch sein, mit fixen inputs (mehr oder weniger inputs rippen das script).
-        deshalb landet der heartbeat count auf der callbacks.py als statische variable und kann von da ausgelesen werden.
-        entsprechend kann auch die callback funktion entsprechend angepasst werden und der wert dort direkt weitergegeben werden.
-        da der loop, wie erwähnt, auch nebenläufig abläuft, könnte der subscriber zu einer statischen klasse verändert und dann mit callbacks zusammengelegt werden.
-        dann passt der subscriber aber nicht mehr zu der daemons idee (siehe movement, lifecycle).
-        """
         self.__running__ = True
         try:
             self.__client__ = Client(client_id=ID_BROKER, clean_session=False)
