fec844bf2391cbff4c973847f3bb08595c009d62
Shooty215 <b.heinr@web.de>
2021-06-22 23:40:28 +0200

    tried to fix no position issue

--- controller_microservice/controller/daemons/callbacks.py
+++ controller_microservice/controller/daemons/callbacks.py
@@ -16,6 +16,9 @@ class Callbacks:
         truckDict = json.loads(payload.decode('utf-8'))
         trucksList = list(Callbacks.truckDictionary.values())
         trucksList.append(truckDict)
+        for i in trucksList:
+            if not type(trucksList[i]['position']) == int:
+                trucksList[i]['position'] = -1
         sortedTrucksList = sorted(trucksList, key=lambda k: k['position'])
         for truck in sortedTrucksList:
             sortedTrucksDict[truck['id']] = truck
